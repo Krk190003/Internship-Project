87f68edf0aa7b7fb6d6b6c4d3655f619d84f17b4
Kokil Kounder <Kokilkounder@gmail.com>
2022-05-30 14:55:51 -0500

    Set up Project and implement Web View

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
+
+
+const URL: string = "https://www.sabre.com/";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+     <View style={{width:"100%", height:"100%"}}>
+        <WebView
+        source={{uri: URL}}
+        onLoad={() => console.log("isloaded")}
+        />
+     </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 30,
   },
 });
